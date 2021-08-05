4ef3cfc10cc15a01766ef77a1d7a6e862b40b95a
Cyan11 <guilhermelacerda620@gmail.com>
2021-08-04 22:20:55 -0300

    Added function for first new letter

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic ;
+
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstNonRepeatingChar(Console.ReadLine()));
+        }
+        static char FirstNonRepeatingChar(string s)
+        {
+            List<char> repeated = new List<char>();
+            List<char> endList = new List<char>();
+            foreach (char c in s)
+            {
+                if (repeated.Contains(c))
+                {
+                    endList.Remove(c);
+                }
+                else
+                {
+                    repeated.Add(c);
+                    endList.Add(c);
+                }
+            }
+            return endList[0];
+        }
+    }
+}
